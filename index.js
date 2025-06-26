f3ba13deb67bfe198e35c4bd5e30055c456764b1
sandeep <sandeep.b@tenmednetwork.com>
2025-06-26 21:03:15 +0530

    added greeting function

---
+++ index.js
@@ -0,0 +1,7 @@
+function greeting() {
+    const greetingMessage = "Hello from the tenMed network — empowering healthcare through innovation."
+    console.log(greetingMessage)
+    return greetingMessage
+}
+
+export default greeting
